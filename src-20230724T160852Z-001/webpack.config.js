5cff07ad1f5be45d687ecd59146fc9c5655c720d
veerajkatti <veeraj178@gmail.com>
2023-07-27 21:46:40 +0530

    takshala build

---
+++ src-20230724T160852Z-001/webpack.config.js
@@ -0,0 +1,38 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: '/home/veeraj/source/takshala/src-20230724T160852Z-001/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
+
